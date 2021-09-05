3cc8616cf4bc403fe445358913ff2b637609d697
Jennet <jennethemrayeva2@gmail.com>
2021-09-05 21:18:12 +0000

    added navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,35 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from './layout.module.css'
+import styles from './navbar.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 const name = 'Your Name'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Navbar({ children, home }) {
   return (
-    <><nav className={styles.navbar}>
+    <>
+    <nav className={styles.navbar}>
           <div className={styles.container}>
               <div className={styles.titleBox}>
                   <p className={styles.title}>harbor</p>
               </div>
-              {/* <ul className={styles.list}> */}
-                  <li className={styles.navItem}><a href="#" className={styles.navLink}>Home <span class="sr-only">(current)</span></a></li>
+              <ul className={styles.list}>
+                  <li className={styles.navItem}><a href="#" className={styles.navLink}>Home</a></li>
                   <li className={styles.navItem}><a href="#" className={styles.navLink}>About</a></li>
                   <li className={styles.navItem}><a href="#" className={styles.navLink}>Know Your Risks</a></li>
                   <li className={styles.navItem}><a href="#" className={styles.navLink}>For Work</a></li>
                   <li className={styles.navItem}><a href="#" className={styles.navLink}>Journal</a></li>
                   <li className={styles.navItem}><a href="#" className={styles.navLink}>Shop</a></li>
-              {/* </ul> */}
+              </ul>
           </div>
       </nav>
-      <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={400}
-              width={400}
-            />
+      
           </>
   )
 }
+
+/*<Image
+priority
+src="/images/profile.jpg"
+className={utilStyles.borderCircle}
+height={400}
+width={400}
+/> */
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
-import Layout from '../components/layout'
+import Navbar from '../components/navbar'
 
 
 export default function Home() {
   return (
-<Layout home>
+    <>
       <Head>
         <title>harbor</title>
       </Head>
+      <Navbar/>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+       
       </section>
-    </Layout>
+      </>
   )
 }
